717eec4d31c270ba8103222349feaae7f5581c90
Francesco4203 <100074926+Francesco4203@users.noreply.github.com>
2023-03-13 13:48:40 +0700

    update default

--- Template/default.cpp
+++ Template/default.cpp
@@ -1,20 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+//#define int long long
 #define x first
 #define y second
 #define pii pair<int, int>
-#define pll pair<long long, long long>
 #define ll long long
 #define pdd pair<double, double>
 #define rp(i, l, r) for (int i = l; i < r; i++)
 
 // Global variables pls
 
-int main()
+singed main()
 {
+    cin.tie(0)->sync_with_stdio(0);
     int test = 1;
-    // cin >> test; if many testcases
+    // cin >> test; //if many testcases
     rp(cas, 0, test)
     {
         // solution
